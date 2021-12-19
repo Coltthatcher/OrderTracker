5042b72bd8a7a4f9980ff0d93e3f65179c415e20
Colt Thatcher <colt.thatcher303@gmail.com>
2021-12-19 14:41:38 -0800

    add in controlles for order, having issues with adding new vendors

--- OrderTracker/Controllers/OrderControllers.cs
+++ OrderTracker/Controllers/OrderControllers.cs
@@ -4,5 +4,24 @@ using System.Collections.Generic;
 
 namespace OrderTracker.Controllers
 {
-  
+  public class OrderController : Controller
+  { 
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> vendorOrders = new Dictionary<string, object>();
+      vendorOrders.Add("order", order);
+      vendorOrders.Add("vendor", vendor);
+      return View(vendorOrders);
+    }
+  }
 }
