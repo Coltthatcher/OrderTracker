fd8e395d85b9a69493b7a0d81c58b913504d5cac
Colt Thatcher <colt.thatcher303@gmail.com>
2021-12-19 14:47:59 -0800

    add namespace and class to itemcontrollers

--- OrderTracker/Controllers/ItemControllers.cs
+++ OrderTracker/Controllers/ItemControllers.cs
@@ -5,5 +5,19 @@ using OrderTracker.Models;
 
 namespace OrderTracker
 {
-  
+  public class VendorController : Controller
+  {
+    public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+}
+
+
+
 }
