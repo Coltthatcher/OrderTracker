2ea4397abdd24351a7242974b9705e1f3e1a4761
Colt Thatcher <colt.thatcher303@gmail.com>
2021-12-19 16:13:24 -0800

    WIP: not all paths are leading to the correct source

--- OrderTracker/Controllers/ItemControllers.cs
+++ OrderTracker/Controllers/ItemControllers.cs
@@ -7,17 +7,49 @@ namespace OrderTracker
 {
   public class VendorController : Controller
   {
-    public class VendorController : Controller
-  {
     [HttpGet("/vendors")]
     public ActionResult Index()
     {
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName,string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{vendorId}")]
+    public ActionResult Show(int vendorId)
+    {
+      Vendor vendors = Vendor.Find(vendorId);
+      Dictionary<string, object> vendorOrders = new Dictionary<string, object>();
+      List<Order> orders = vendors.Orders;
+      vendorOrders.Add("vendors", vendors);
+      vendorOrders.Add("orders", orders);
+      return View(vendorOrders);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, string orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor thisVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      thisVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = thisVendor.Orders;
+      model.Add("Orders", vendorOrders);
+      model.Add("vendor", thisVendor);
+      return View("Show", model);
+    }
+
   }
-}
-
-
-
 }
--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -8,7 +8,7 @@ namespace OrderTracker.Models
 
     public string Description { get; set; }
     public string Name { get; set; }
-    public int Id { get; }
+    public int Id { get; set;}
     public List<Order> Orders { get; set; }
 
     public Vendor(string vendorName, string vendorDescription)
